046976aaeb68f411e18935003d2246cff5dab3ff
zhaofm <zhaofm@mail.jj.cn>
2020-02-14 02:01:09 +0800

    [第一行android]第四章fragment,新闻显示

---
+++ app/src/main/java/com/example/root/studyview/News/News.java
@@ -0,0 +1,30 @@
+package com.example.root.studyview.News;
+
+/**
+  *
+  * @ClassName:      News
+  * @Description:   新闻类
+  * @Author:         zfm
+  * @CreateDate:     2020/2/13 22:18
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/com/example/root/studyview/News/NewsContentFragment.java
@@ -0,0 +1,44 @@
+package com.example.root.studyview.News;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+
+import com.example.root.studyview.R;
+
+
+/**
+  *
+  * @ProjectName:
+  * @Package:        com.example.root.studyview
+  * @ClassName:      NewsContentFragment
+  * @Description:     java类作用描述
+  * @Author:         ZFM
+  * @CreateDate:     2020/2/13 22:49
+ */
+
+public class NewsContentFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_content_frag, container,false);
+        return view;
+    }
+
+    public void refresh(String newsTitle, String newsContent){
+        View visibilitylayout = view.findViewById(R.id.visibility_layout);
+        visibilitylayout.setVisibility(View.VISIBLE);
+        TextView newsTitileView = (TextView)view.findViewById(R.id.news_title);
+        TextView newsContentView = (TextView)view.findViewById(R.id.news_content);
+        newsTitileView.setText(newsTitle);
+        newsContentView.setText(newsContent);
+    }
+
+}
